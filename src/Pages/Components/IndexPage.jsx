930843729417a89336f49e9e6c97a70818cb1517
Edgar Talledos <onimenotsuki@gmail.com>
2018-07-11 19:57:07 -0500

    Corregido margin

--- src/Pages/Components/IndexPage.jsx
+++ src/Pages/Components/IndexPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const IndexPage = () => (
-  <div className="mdc-layout-grid">
+  <div
+    className="mdc-layout-grid"
+  >
     <div className="mdc-layout-grid__inner">
       <div className="mdc-layout-grid__cell">
         Amet justo donec enim diam!
--- src/Shared/Components/Header.jsx
+++ src/Shared/Components/Header.jsx
@@ -5,12 +5,12 @@ import '@material/top-app-bar/dist/mdc.top-app-bar.min.css';
 
 const Header = () => (
   <header
-    class="mdc-top-app-bar"
+    className="mdc-top-app-bar"
   >
-    <div class="mdc-top-app-bar__row">
-      <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-        <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">menu</a>
-        <span class="mdc-top-app-bar__title">BookStore</span>
+    <div className="mdc-top-app-bar__row">
+      <section className="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+        <a href="#" className="material-icons mdc-top-app-bar__navigation-icon">menu</a>
+        <span className="mdc-top-app-bar__title">BookStore</span>
       </section>
     </div>
   </header>
