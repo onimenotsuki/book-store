30fe935c0e1c2dd79dbaba28430178d91eb2ed34
Edgar Talledos <onimenotsuki@gmail.com>
2018-07-16 19:57:46 -0500

    Añadidas cartas de libros

--- src/Pages/Components/Book.jsx
+++ src/Pages/Components/Book.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
 
+import '@material/card/dist/mdc.card.min.css';
+
 const Book = (props) => (
     <div
         key={props.id}
         className="mdc-layout-grid__cell"
         position={props.position}
     >
-        <h1>
-            {props.title}
-        </h1>
+        <div className="mdc-card">
+            <div className="mdc-card__media mdc-card__media--square">
+                <div className="mdc-card__media-content">
+                    <h1>
+                        {props.title}
+                    </h1>
 
-        <p className={props.existences == 0 ? 'no-existences' : 'existences'}>
-            {props.existences == 0 ? 'no hay disponibles' : props.existences}
-        </p>
+                    <p className={props.existences === 0 ? 'no-existences' : 'existences'}>
+                        {props.existences === 0 ? 'no hay disponibles' : props.existences}
+                    </p>
 
-        <p className="author">
-            {props.author}
-        </p>
+                    <p className="author">
+                        {props.author}
+                    </p>
 
-        <p className="description">
-            {props.description || props.children}
-        </p>
+                    <p className="description">
+                        {props.description || props.children}
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
--- src/Pages/Components/IndexPage.jsx
+++ src/Pages/Components/IndexPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import BookList from './BookList'; 
+import '@material/layout-grid/dist/mdc.layout-grid.min.css';
 
 const IndexPage = () => (
   <div
