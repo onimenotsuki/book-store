7fe857871229accbc260b6cbabae0a8123156b66
Edgar Talledos <onimenotsuki@gmail.com>
2018-07-25 19:22:46 -0500

    Añadidos ciclos para renderizar usuario

--- src/Pages/Components/ProfilePage.jsx
+++ src/Pages/Components/ProfilePage.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 
 class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      user: {
+        name: "Roberto Nuñez",
+        age: 45,
+        title: "Senior",
+      },
+    });
+  }
+
   render() {
+    console.log(this.state.user);
+
     return (
       <div className="mdc-layout-grid">
-        <div className="mdc-card">
-          <h1>Esta es la página de perfil</h1>
+        <div className="mdc-card" style={{ marginTop: 80, padding: 20 }}>
+          <h2>{this.state.user.name}</h2>
         </div>
       </div>
     );
