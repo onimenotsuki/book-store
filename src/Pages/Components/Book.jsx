8ba4d7b23d2a4aedac2dd5bc8ab8082499a83e66
Edgar Talledos <onimenotsuki@gmail.com>
2018-07-26 19:25:35 -0500

    Modificadas cards para conectarse a la API

--- src/Pages/Components/Book.jsx
+++ src/Pages/Components/Book.jsx
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import '@material/card/dist/mdc.card.min.css';
 
 // Componentes
 import Ranking from './Ranking';
 
-const Book = (props) => (
-  <div className="mdc-layout-grid__cell">
-    <div
-      className="mdc-card book-card"
-      style={{ marginTop: 80, padding: 20 }}
-    >
-      <div className="mdc-card__media">
-        <h1 className="title">
-          {props.title}
-        </h1>
+class Book extends Component {
+  constructor() {
+    super();
+    this.state = {
+      author: {}
+    };
+  }
 
-        <p className={props.existences === 0 ? 'no-existences' : 'existences'}>
-          {props.existences === 0 ? 'no hay disponibles' : props.existences}
-        </p>
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/users/' + this.props.authorId)
+      .then(response => {
+        this.setState({
+          author: response.data,
+        });
+      });
+  }
 
-        <p className="author">
-          {props.author}
-        </p>
+  render() {
+    return (
+      <div className="mdc-layout-grid__cell">
+        <div
+          className="mdc-card book-card"
+          style={{ marginTop: 80, padding: 20 }}
+        >
+          <div className="mdc-card__media">
+            <h1 className="title">
+              {this.props.title}
+            </h1>
 
-        <p className="description">
-          {props.description || props.children}
-        </p>
+            <p className={this.props.existences === 0 ? 'no-existences' : 'existences'}>
+              {this.props.existences === 0 ? 'no hay disponibles' : this.props.existences}
+            </p>
 
-        <Ranking />
+            <p className="author">
+              {this.state.author.name}
+            </p>
+
+            <p className="description">
+              {this.props.description || this.props.children}
+            </p>
+
+            <Ranking />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Book;
--- src/Pages/Components/BookList.jsx
+++ src/Pages/Components/BookList.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Book from './Book';
-import LastBook from './LastBook';
 
-const books = [
-  {
-    title: 'El aleph',
-    author: 'Jorge Luis Borges',
-    existences: 7,
-    description: 'El Aleph es uno de los libros de cuentos más representativos del escritor argentino Jorge Luis Borges. Publicado en 1949, fue reeditado por el autor en 1974. Sus textos remiten a una infinidad de fuentes y bibliografías en torno a las cuales se articulan mitos y metáforas de la tradición literaria universal. ',
-  }, {
-    title: 'Cien años de soledad',
-    author: 'Gabriel García Márquez',
-    existences: 0,
-    description: 'Es considerada una obra maestra de la literatura hispanoamericana y universal, así como una de las obras más traducidas y leídas en español.1​ Fue catalogada como una de las obras más importantes de la lengua castellana durante el IV Congreso Internacional de la Lengua Española celebrado en Cartagena de Indias en marzo de 2007.',
-  }, {
-    title: 'The Long Tail',
-    author: 'Chris Anderson',
-    existences: 1,
-    description: 'The book argues that products in low demand or that have a low sales volume can collectively build a better market share than their rivals, or exceed the relatively few current bestsellers and blockbusters, provided the store or distribution channel is large enough.',
-  },
-];
+class BookList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: [
+        {
+          title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+          authorId: 1,
+          description: 'Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Fusce sagittis, libero non molestie mollis, magna orci ultrices dolor, at vulputate neque nulla lacinia eros.  Sed diam.',
+        },
+      ],
+    };
+  }
 
-const BookList = () => (
-  <div className="mdc-layout-grid__inner">
-    {books.map((book, index) => {
-       return (
-         <Book
-           title={book.title}
-           key={index}
-           position={index}
-           author={book.author}
-           description={book.description}
-         />
-       );
-    })}
-  </div>
-);
+  render() {
+    return (
+      <div className="mdc-layout-grid__inner">
+        {this.state.books.map((book, index) => {
+           return (
+             <Book
+               title={book.title}
+               key={index}
+               position={index}
+               authorId={book.authorId}
+               description={book.description}
+             />
+           );
+        })}
+      </div>
+    );
+  }
+}
 
 export default BookList;
